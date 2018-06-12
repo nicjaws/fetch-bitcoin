9f3373e99830012b6eed193d40ae30bc5d413ed1
Nicolas Corbalan <outreef@hotmail.com>
2018-06-12 11:55:43 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import FetchExample from './sections/fetch-example';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FetchExample />
+      </div>
+    );
+  }
+}
+
+export default App;
